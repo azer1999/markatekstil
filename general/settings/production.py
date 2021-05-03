5ce98fa74f791b72a99df959cd5ec2c3de73b1fc
pylvin <elxjd.2014@gmail.com>
2021-05-03 16:14:13 +0400

    init

--- general/settings/production.py
+++ general/settings/production.py
@@ -18,3 +18,21 @@ DATABASES = {
         'PORT': config('POSTGRES_PORT'),
     }
 }
+
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://8c57cd85801b4b5290ef21e02c3304c7@o551554.ingest.sentry.io/5746965",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
